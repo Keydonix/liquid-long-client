b7b81336f33d6b18401f9f5b21ee470d464480e9
Micah Zoltu <micah@zoltu.net>
2019-01-16 21:42:22 +0800

    Makes the max leverage size dynamic based on contract balance.
    Note: Depends on contract changes getting merged and deployed.

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
 import { MiddlewareService } from '../../services/middleware/middleware.service';
 import { combineLatest } from 'rxjs';
 import { catchError, distinctUntilChanged, map, shareReplay, startWith } from 'rxjs/operators';
@@ -14,15 +13,18 @@ import { LoggerService } from '../../services/logger/logger.service';
 })
 export class MainPageComponent implements OnInit {
 
-  maxQuantity = environment.maxQuantity;
-  price$ = this.middleware.price$
-    .pipe(
-      catchError((err, caught) => {
-        this.logger.error(`middleware.price$ error:`, err);
-        this.isNotConnected = true;
-        return caught;
-      }),
-    );
+  maxQuantity$ = this.middleware.maxLeverageSize$.pipe(
+    map(x => MainPageComponent.toSignificantFigures(x, 2, Math.floor)),
+    shareReplay(1),
+  );
+  price$ = this.middleware.price$.pipe(
+    catchError((err, caught) => {
+      this.logger.error(`middleware.price$ error:`, err);
+      this.isNotConnected = true;
+      return caught;
+    }),
+    shareReplay(1),
+  );
 
   isDebug = false;
   isWrongBrowser = !MiddlewareService.isWeb3Compatible();
@@ -120,6 +122,27 @@ export class MainPageComponent implements OnInit {
         this.confirmEmitter$.emit(true);
         this.router.navigateByUrl('/fail');
       });
+  }
+
+  static toSignificantFigures(value: number, numberOfSignificantFigures: number, roundingFunction: (number: number) => number): number {
+    // early return for 0
+    if (value === 0) {
+      return 0;
+    }
+    // find the first significant digit
+    for (let i = 0; i < 100; ++i) {
+      const mostSignificantFigure = i - 50;
+      // this wasn't it, try the next one
+      if (Math.floor(value * 10 ** mostSignificantFigure) === 0) {
+        continue;
+      }
+      // we found the most significant digit, return `numberOfSignificantFigures` more figures after that (inclusive)
+      const leastSignificantFigure = mostSignificantFigure + numberOfSignificantFigures - 1;
+      // we round by getting the digit we care about into the 1s place, and then calling the rounding function, then adding an appropriate number of zeros back
+      return roundingFunction(value * 10 ** leastSignificantFigure) * 10 ** (-leastSignificantFigure);
+    }
+    // only reachable if the value is _really_ close to 0
+    return 0;
   }
 
 }
