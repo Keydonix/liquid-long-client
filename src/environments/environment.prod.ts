d292e8f1c289439645e44f890ad5926f2f224446
Micah Zoltu <micah@zoltu.net>
2019-03-15 18:51:30 +0800

    Adds links out to new closure UI and updates contract address.
    Due to some contract changes, we had to update the client library which went through a few major revisions, so there is minor work to address API changes.
    
    Fixed the cursor when hovering over the close button.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
+import { Address } from "@keydonix/liquid-long-client-library";
+
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: true,
   minQuantity: 0.001, // min value and step size
-  liquidLongContractAddress: '0x69E51C852FB4D7A8C47BDF438B115AD13A6BFC52',
+  liquidLongContractAddress: Address.fromHexString('0xADbE0dD80f0D75BEeD3247d659Ff7FE3bDACD8a1'),
   defaultEthPriceInUsd: 150,
   jsonRpcAddress: 'https://eth-mainnet.alchemyapi.io/jsonrpc/7sE1TzCIRIQA3NJPD5wg7YRiVjhxuWAE',
   defaultProviderFeeRate: 0.01,
